0242b49d80a4bd4d18c465b34f9fadb6723d9299
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2020-12-07 18:20:51 +0300

    added a test for save and retrive all fields enterted

--- src/test/java/models/SightingTest.java
+++ src/test/java/models/SightingTest.java
@@ -21,6 +21,22 @@ public class SightingTest {
     }
 
 
+    @Test
+    public void  save_insertObjectsToTheInstanceOfSighting_true(){
+        sighting testSighting = setUpNewSighting();
+        testSighting.save();
+        assertTrue(sighting.all().get(0).equals(testSighting));
+    }
+
+    @Test
+    public void all_returnsAllInstancesOfSighting_true(){
+        sighting firstSighting = setUpNewSighting();
+        firstSighting.save();
+        sighting secondSighting = new sighting("Njenga","Zone B",2,"common");
+        secondSighting.save();
+        assertEquals(true,sighting.all().get(0).equals(firstSighting));
+        assertEquals(true,sighting.all().get(1).equals(secondSighting));
+    }
     public sighting setUpNewSighting() {return new sighting()}
 
 
