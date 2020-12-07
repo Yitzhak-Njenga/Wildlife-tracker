0f9f399046f9870cc2ae90b42fab9cd3bea64aa5
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2020-12-07 09:49:58 +0300

    added sighting variables

--- src/main/java/models/sighting.java
+++ src/main/java/models/sighting.java
@@ -1,4 +1,15 @@
 package models;
 
+import java.sql.Timestamp;
+
 public class sighting {
+    private String location;
+    private String name;
+    private int animalId;
+    private int id;
+    private String type;
+    private String age;
+    private String health;
+    public Timestamp timestamp;
+
 }
