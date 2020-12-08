f6215678460cf83d892173bdb8eddbc321d1fb17
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2020-12-08 15:33:41 +0300

    aded a rule for the endangered aninmals class

--- src/main/java/models/endangeredAnimal.java
+++ src/main/java/models/endangeredAnimal.java
@@ -1,4 +1,68 @@
 package models;
 
-public class endangeredAnimal {
+import java.util.List;
+import java.util.Objects;
+import org.sql2o.*;
+import java.sql.Timestamp;
+
+public class endangeredAnimal extends Animals {
+    private String health;
+    private String age;
+    private String type;
+
+    public static final String ANIMAL_TYPE = "Endangered";
+
+
+    public endangeredAnimal(String name, String health, String age) {
+        super(name);
+        if (name.equals("") || health.equals("") || age.equals("")) {
+            throw new IllegalArgumentException("Please input all fields as required");
+        }
+        this.health = health;
+        this.age = age;
+        type = ANIMAL_TYPE;
+    }
+
+
+    @Override
+    public boolean equals(Object otherAnimal) {
+        if (this == otherAnimal) return true;
+        if (otherAnimal == null || getClass() != otherAnimal.getClass()) return false;
+        endangeredAnimal animal = (endangeredAnimal) otherAnimal;
+        return (this.name.equals(animal.name));
+    }
+
+
+    public void save() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO endangered_animals (name,type,health,age) VALUES (:name,:type,:health,:age)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .addParameter("type", this.type)
+                    .addParameter("health", this.health)
+                    .addParameter("age", this.age)
+                    .throwOnMappingFailure(false)
+                    .executeUpdate()
+                    .getKey();
+
+
+        }
+    }
+
+    public static List<endangeredAnimal> getAll() {
+        String sql = "SELECT * FROM endangered_animals ORDER BY id ASC";
+        try (Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(endangeredAnimal.class);
+        }
+    }
+
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
 }
---
+++ src/test/java/models/endangeredAnimalTest.java
@@ -0,0 +1,18 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class endangeredAnimalTest {
+
+    @Rule
+    public DataBaseRule database = new DataBaseRule();
+
+
+
+
+
+}
