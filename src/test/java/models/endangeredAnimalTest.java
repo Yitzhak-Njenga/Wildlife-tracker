1ee3876c8d66e5ec6445af8772a05aa96bb8351d
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2020-12-08 15:37:58 +0300

    updated endangered animal class test

--- src/test/java/models/endangeredAnimalTest.java
+++ src/test/java/models/endangeredAnimalTest.java
@@ -11,8 +11,56 @@ public class endangeredAnimalTest {
     @Rule
     public DataBaseRule database = new DataBaseRule();
 
+    @Test
+    public void animals_instantiatesCorrectly_true() {
+        endangeredAnimal testEndangered = setUpNewEndangered();
+        assertTrue(testEndangered instanceof endangeredAnimal);
+    }
 
+    @Test
+    public void getName_animalsInstantiatesWithName_true() {
+        endangeredAnimal testEndangered = setUpNewEndangered();
+        assertEquals("Baboon", testEndangered.getName());
 
+    }
+
+    @Test
+    public void returnsTrueIfNamesAreTheSame_true() {
+        endangeredAnimal testEndangered = setUpNewEndangered();
+        endangeredAnimal anotherEndangered = new endangeredAnimal("Baboon","healthy","adult");
+        assertTrue(testEndangered.equals(anotherEndangered));
+    }
+
+    @Test
+    public void save_insertsObjectIntoDatabase_true() {
+        endangeredAnimal testEndangered = setUpNewEndangered();
+        testEndangered.save();
+        System.out.println(testEndangered);
+        assertTrue(endangeredAnimal.getAll().get(0).equals(testEndangered));
+    }
+
+    @Test
+    public void getAll_returnsAllInstancesOfAnimals_true() {
+        endangeredAnimal testEndangered = setUpNewEndangered();
+        testEndangered.save();
+        endangeredAnimal secondEndangered = new endangeredAnimal("Lion","ill","newborn");
+        secondEndangered.save();
+        assertEquals(true,endangeredAnimal.getAll().get(0).equals(testEndangered));
+        assertEquals(true, endangeredAnimal.getAll().get(1).equals(secondEndangered));
+    }
+
+    @Test
+    public void save_assignsIdToObject_true() {
+        endangeredAnimal testEndangered = setUpNewEndangered();
+        testEndangered.save();
+        endangeredAnimal secondEndangered = endangeredAnimal.getAll().get(0);
+        assertEquals(testEndangered.getId(),secondEndangered.getId());
+
+    }
+
+    public endangeredAnimal setUpNewEndangered(){
+        return new endangeredAnimal ("Baboon","ill","adult");
+    }
 
 
 }
