616e63e45af99f5407ab89cce70ad924879d2ece
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2020-12-08 19:45:31 +0300

    updated endangered animal class test

--- src/main/java/models/location.java
+++ src/main/java/models/location.java
@@ -1,4 +1,41 @@
 package models;
+import org.sql2o.Connection;
+
+import java.util.List;
+import java.util.Objects;
 
 public class location {
+    private String name;
+    private int id;
+
+    public  location(int id,String name){
+        this.id = id;
+        this.name = name;
+
+    }
+
+
+
+    public static List <location> all() {
+        String sql = "SELECT * FROM locations  ORDER BY id ASC";
+        try(Connection con = DB.sql2o.open()){
+            return con.createQuery(sql).executeAndFetch(location.class);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/main/java/models/ranger.java
+++ src/main/java/models/ranger.java
@@ -1,4 +1,41 @@
 package models;
 
+import org.sql2o.Connection;
+
+import java.util.List;
+import java.util.Objects;
+
 public class ranger {
+    public String name;
+    public int id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ranger ranger = (ranger) o;
+        return id == ranger.id &&
+                Objects.equals(name, ranger.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id);
+    }
+
+    public static List<ranger> all() {
+        String sql = "SELECT * FROM rangers";
+        try(Connection con = DB.sql2o.open()){
+            return con.createQuery(sql).executeAndFetch(ranger.class);
+        }
+    }
+
+    public String getName() {
+        return name ;
+    }
+
+    public int getId() {
+        return id;
+    }
+
 }
